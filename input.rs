4cd555165f0cd1479c2eeddd88a72c8f70e21790
michael-hiegl <100510019+michael-hiegl@users.noreply.github.com>
2022-10-18 11:10:00 +0200

    Update input.rs

--- input.rs
+++ input.rs
@@ -3,7 +3,7 @@ use std::io;
 pub fn getstr(writeto: &mut String)
 {
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).ok().expect("Not a valid input");
     *writeto = input;
 }
 
@@ -14,7 +14,7 @@ pub mod getnum
     pub fn i128(writeto: &mut i128)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
@@ -22,7 +22,7 @@ pub mod getnum
     pub fn i64(writeto: &mut i64)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
@@ -30,7 +30,7 @@ pub mod getnum
     pub fn i32(writeto: &mut i32)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
@@ -38,7 +38,7 @@ pub mod getnum
     pub fn i16(writeto: &mut i16)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
@@ -46,7 +46,7 @@ pub mod getnum
     pub fn i8(writeto: &mut i8)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
@@ -54,7 +54,7 @@ pub mod getnum
     pub fn f64(writeto: &mut f64)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
@@ -62,7 +62,7 @@ pub mod getnum
     pub fn f32(writeto: &mut f32)
     {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).ok().expect("Not a valid input");
         let number = input.trim().parse().expect("Not a valid input");
         *writeto = number;
     }
