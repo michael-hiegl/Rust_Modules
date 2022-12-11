7630612e47516f20e45a6f0bc92397b97e2c147b
michael-hiegl <100510019+michael-hiegl@users.noreply.github.com>
2022-12-11 15:58:16 +0100

    Update input.rs

--- input.rs
+++ input.rs
@@ -1,5 +1,12 @@
 use std::io;
 
+///will store the input in the given argument
+/// 
+///needs a mutable reference to that variable
+/// 
+///works like scanf() from C
+/// 
+///for example getstr(&mut x) will store the user input into x
 pub fn getstr(writeto: &mut String)
 {
     let mut input = String::new();
@@ -7,10 +14,18 @@ pub fn getstr(writeto: &mut String)
     *writeto = input;
 }
 
+///will store the input in the given argument
+/// 
+///needs a mutable reference to that variable
+/// 
+///works like scanf() from C
+/// 
+///for example getnum::i32(&mut x) will store the user input into x
 pub mod getnum
 {
     use std::io;
     
+    ///for explanation look at getnum
     pub fn i128(writeto: &mut i128)
     {
         let mut input = String::new();
@@ -19,6 +34,7 @@ pub mod getnum
         *writeto = number;
     }
 
+    ///for explanation look at getnum
     pub fn i64(writeto: &mut i64)
     {
         let mut input = String::new();
@@ -27,6 +43,7 @@ pub mod getnum
         *writeto = number;
     }
 
+    ///for explanation look at getnum
     pub fn i32(writeto: &mut i32)
     {
         let mut input = String::new();
@@ -35,6 +52,7 @@ pub mod getnum
         *writeto = number;
     }
 
+    ///for explanation look at getnum
     pub fn i16(writeto: &mut i16)
     {
         let mut input = String::new();
@@ -43,6 +61,7 @@ pub mod getnum
         *writeto = number;
     }
 
+    ///for explanation look at getnum
     pub fn i8(writeto: &mut i8)
     {
         let mut input = String::new();
@@ -51,6 +70,7 @@ pub mod getnum
         *writeto = number;
     }
 
+    ///for explanation look at getnum
     pub fn f64(writeto: &mut f64)
     {
         let mut input = String::new();
@@ -59,6 +79,7 @@ pub mod getnum
         *writeto = number;
     }
 
+    ///for explanation look at getnum
     pub fn f32(writeto: &mut f32)
     {
         let mut input = String::new();
